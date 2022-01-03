5729eebe04565b970895ee416e1f95aa9b3f74d7
littlewhite33z <1021014610@qq.com>
2022-01-03 10:51:21 +0800

    实现组件emits

---
+++ src/runtime-core/componentEmit.ts
@@ -0,0 +1,39 @@
+import { camelize, capitalize, toHandlerKey } from "../shared/index";
+
+export function emit(instance, event, ...args) {
+  // console.log("emit" + event);
+
+  //  instance.props  有没有对应event的回调
+  const { props } = instance;
+
+  //tpp  ->
+  //先去写一个特定行为 -》 重构通用行为
+
+  // const handler = props["onAdd"];
+
+  // //add-foo ->addFoo
+  // const camelize = (str: string) => {
+  //   return str.replace(/-(\w)/g, (_, c: string) => {
+  //     return c ? c.toUpperCase() : "";
+  //   });
+  // };
+  // //addFoo ->AddFoo
+  // const capitalize = (str: string) => {
+  //   return str.charAt(0).toUpperCase() + str.slice(1);
+  // };
+  // // console.log(capitalize(event));
+
+  // const str = capitalize(camelize(event));
+  // // AddFoo -> toAddFoo
+  // const toHandlerKey = (str: string) => {
+  //   return str ? "on" + str : "";
+  // };
+  //add-foo -> addFoo
+  let str = camelize(event);
+
+  //addFoo -> AddFoo
+  str = capitalize(str);
+  const handlerName = toHandlerKey(str);
+  const handler = props[handlerName];
+  handler && handler(...args);
+}
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -6,3 +6,20 @@ export function isObject(val) {
 
 export const hasOwn = (val, key) =>
   Object.prototype.hasOwnProperty.call(val, key);
+
+//add-foo ->addFoo
+export const camelize = (str: string) => {
+  return str.replace(/-(\w)/g, (_, c: string) => {
+    return c ? c.toUpperCase() : "";
+  });
+};
+//addFoo ->AddFoo
+export const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+// console.log(capitalize(event));
+
+// AddFoo -> toAddFoo
+export const toHandlerKey = (str: string) => {
+  return str ? "on" + str : "";
+};
