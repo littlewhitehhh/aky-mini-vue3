00b938f522b4482d3005bb4238de4a209b9d5bf3
littlewhite33z <1021014610@qq.com>
2022-02-20 19:59:51 +0800

    中间部分的删除、移动、增加  最长递增子序列

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -279,6 +279,83 @@ export function createRenderer(options) {
         hostRemove(c1[i].el);
         i++;
       }
+    } else {
+      //中间对比
+      let s1 = i;
+      let s2 = i;
+      let patched = 0;
+      const toBePatch = e2 - s2 + 1;
+      //建立新child.key的映射表
+      const keyToNewIndexMap = new Map();
+
+      //初始化映射表
+      const newIndexToOldIndexMap = new Array(toBePatch);
+
+      let moved = false;
+      let maxNewIndexSoFar = 0;
+      for (let i = 0; i < toBePatch; i++) {
+        newIndexToOldIndexMap[i] = 0;
+      }
+      for (let i = s2; i <= e2; i++) {
+        let nextChild = c2[i];
+        keyToNewIndexMap.set(nextChild.key, i);
+      }
+
+      for (let i = s1; i <= e1; i++) {
+        const prevChild = c1[i];
+
+        if (patched >= toBePatch) {
+          hostRemove(prevChild.el);
+          continue;
+        }
+
+        let newIndex;
+        if (prevChild.key != null) {
+          //有key情况
+          newIndex = keyToNewIndexMap.get(prevChild.key);
+        } else {
+          //无key
+          for (let j = s2; j < e2; j++) {
+            if (isSameVnodeType(prevChild, c2[j])) {
+              newIndex = j;
+              break;
+            }
+          }
+        }
+
+        if (newIndex === undefined) {
+          hostRemove(prevChild.el);
+        } else {
+          if (newIndex >= maxNewIndexSoFar) {
+            maxNewIndexSoFar = newIndex;
+          } else {
+            moved = true;
+          }
+          newIndexToOldIndexMap[newIndex - s2] = i + 1;
+
+          patch(prevChild, c2[newIndex], container, parentComponent, null);
+          patched++;
+        }
+      }
+
+      const increasingNewIndexSequence = moved ? getSequence(newIndexToOldIndexMap) : [];
+      let j = increasingNewIndexSequence.length - 1;
+
+      for (let i = toBePatch - 1; i >= 0; i--) {
+        const nextIndex = i + s2;
+        const nextChild = c2[nextIndex];
+        const anchor = nextIndex + 1 < c2.length ? c2[nextIndex + 1].el : null;
+        if (newIndexToOldIndexMap[i] === 0) {
+          patch(null, nextChild, container, parentComponent, anchor);
+        } else if (moved) {
+          if (j < 0 || i !== increasingNewIndexSequence[j]) {
+            console.log("移动位置");
+            hostInsert(nextChild.el, container, anchor);
+          } else {
+            j--;
+          }
+        }
+      }
     }
   }
 
@@ -335,4 +412,46 @@ export function createRenderer(options) {
   return {
     createApp: createAppAPI(render),
   };
+}
+
+//最强递增子序列
+function getSequence(arr) {
+  const p = arr.slice();
+  const result = [0];
+  let i, j, u, v, c;
+  const len = arr.length;
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i];
+    if (arrI !== 0) {
+      j = result[result.length - 1];
+      if (arr[j] < arrI) {
+        p[i] = j;
+        result.push(i);
+        continue;
+      }
+      u = 0;
+      v = result.length - 1;
+      while (u < v) {
+        c = (u + v) >> 1;
+        if (arr[result[c]] < arrI) {
+          u = c + 1;
+        } else {
+          v = c;
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1];
+        }
+        result[u] = i;
+      }
+    }
+  }
+  u = result.length;
+  v = result[u - 1];
+  while (u-- > 0) {
+    result[u] = v;
+    v = p[v];
+  }
+  return result;
 }
